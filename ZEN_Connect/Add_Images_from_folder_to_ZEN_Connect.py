a1952f90c6b030526f0de6a8cc6b79ee7fa51f6a
sebi06 <3833249+sebi06@users.noreply.github.com>
2022-01-11 08:42:31 +0100

    Added new script and updates for ZENConnect_SaveAllImagesAsCWS.py

---
+++ ZEN_Connect/Add_Images_from_folder_to_ZEN_Connect.py
@@ -0,0 +1,112 @@
+﻿#################################################################
+# File        : ZEN_Connect_Add_Images_from_folder.py
+# Version     : 0.1
+# Author      : czjuz
+# Date        : 04.01.2021
+# Institution : Carl Zeiss Microscopy GmbH
+#
+# - Add all images found in a folder to a ZEN Connect project
+# - Do this recursively as option
+#
+# Use with ZEN Blue 3.4 or newer
+# Copyright(c) 2022 Carl Zeiss AG, Germany. All Rights Reserved.
+#
+# Permission is granted to use, modify and distribute this code,
+# as long as this copyright notice remains part of the code.
+#################################################################
+
+version = 0.1
+
+from System.IO import Path
+from datetime import datetime
+from time import gmtime, strftime
+from System import ApplicationException
+import os
+FILEWARNING = 50
+
+# count the number of czi files
+def CountCZIFiles(basedirectory):
+    czicounter = 0
+    dirnum = 0
+    filenum = 0
+    for root, dirs, files in os.walk(basedirectory):
+        for elements in files:
+            if elements.endswith(".czi"):
+                czicounter+=1
+        dirnum += len(dirs)
+        filenum += len(files)
+    
+    return dirnum, filenum, czicounter
+    
+# clear output
+Zen.Application.MacroEditor.ClearMessages()
+
+# close a project, if one is open
+OldDocument = ZenConnect.GetActiveZenConnectDocument()
+if not OldDocument is None:
+    OldDocument.CloseZenConnectDocument(False)
+
+
+
+# generate dialog
+initialpath = os.path.join(os.getenv('Userprofile'), "Documents")
+CWSDialog = ZenWindow()
+CWSDialog.Initialize('Add all images from the selected folder to a new ZEN Connect workspace - Version: ' + str(version))
+CWSDialog.AddFolderBrowser('projectfolder', 'ZEN Connect Project Target Folder:','',"0","0")
+CWSDialog.AddFolderBrowser('imagefolder', 'Folder with images:', initialpath, "0", "1")
+CWSDialog.AddCheckbox('recursive', 'Include images of all subdirectories', False, "1", "0")
+CWSDialog.AddCheckbox('closecws', 'Close CWS project at the end', False, "2", "0")
+sf = CWSDialog.Show()
+
+if sf.HasCanceled:
+    message = 'Macro was canceled by user.'
+    print(message)
+    raise SystemExit
+
+# Get resulsts from window
+targetprojectfolder = str(sf.GetValue('projectfolder'))
+print('Using project target folder: ', targetprojectfolder)
+imagefolder = str(sf.GetValue('imagefolder'))
+print ('Using images from folder: ', imagefolder)
+recursive = sf.GetValue('recursive')
+closecwsproject = sf.GetValue('closecws')
+
+# Generates a warning if a high number of CZI images is found
+if recursive:
+    fileresults = (CountCZIFiles(imagefolder))
+    if fileresults[2] > FILEWARNING:
+        warndialog = ZenWindow()
+        warndialog.HasCancelButton = True
+        warndialog.HasOkButton = True
+        warndialog.AddLabel("Found more than " + str(fileresults[2]) + " files in " + str(fileresults[0]) + " folders" + "\r\n" + "in folder " +  imagefolder  + "\r\n" + "Would you like to continue ")
+        a = warndialog.Show()
+        if a.HasCanceled:
+            message = 'Macro was canceled by user.'
+            print(message)
+            raise SystemExit
+
+# create CWS project name based on date
+projectname = strftime('ZEN_Connect_%Y-%m-%d_%H-%M', gmtime())
+fullprojectname = Path.Combine(targetprojectfolder, projectname) + ".a5proj"
+print('Created new project name: ', fullprojectname)
+
+# Create the project in the folder below 
+NewProject = ZenConnect.CreateZenConnectDocument(fullprojectname)
+cwsdata_path = Path.Combine(targetprojectfolder, projectname) + '_data'
+
+results = []
+if recursive:
+    results = NewProject.AddImagesInFolderToZenConnectDocument(imagefolder, True)  
+else:
+    results = NewProject.AddImagesInFolderToZenConnectDocument(imagefolder, False)  
+
+# print files that could not be added
+for i in results:
+    print ("could not be added: ", i)
+
+# save CSW project at the end
+NewProject.SaveZenConnectDocument()
+
+# close CWS project at the end
+if closecwsproject:
+    NewProject.CloseZenConnectDocument(False)
---
+++ ZEN_Connect/ZENConnect_SaveAllImagesAsCWS.py
@@ -0,0 +1,125 @@
+﻿#################################################################
+# File        : ZEN_Connect_SaveAllImagesAsCWS.py
+# Version     : 0.2
+# Author      : czsrh, czswg, czjuz
+# Date        : 26.04.2021
+# Institution : Carl Zeiss Microscopy GmbH
+#
+# - Save all open image documents to a new CWS project
+# - Check if image contains preview or label images and add them as well
+#
+# Use with ZEN Blue 3.4 or newer
+# Copyright(c) 2021 Carl Zeiss AG, Germany. All Rights Reserved.
+#
+# Permission is granted to use, modify and distribute this code,
+# as long as this copyright notice remains part of the code.
+#################################################################
+
+version = 0.2
+
+from System.IO import Path, File, Directory, FileInfo, FileStream, FileMode, FileAccess, FileShare, FileOptions
+from datetime import datetime
+from time import gmtime, strftime
+from System import ApplicationException
+import os
+
+def GetImageDocument(attachment, name):
+    fst = FileStream(name, FileMode.Create, FileAccess.ReadWrite,FileShare.Read, 4096, FileOptions.SequentialScan)
+    a.WriteToStream(fst)
+    fst.Close()
+
+# clear output
+Zen.Application.MacroEditor.ClearMessages()
+
+# close a project, if one is open
+OldDocument = ZenConnect.GetActiveZenConnectDocument()
+if not OldDocument is None:
+    OldDocument.CloseZenConnectDocument(False)
+
+
+imagefiles = []
+
+initialpath = os.path.join(os.getenv('Userprofile'), "Documents")
+CWSDialog = ZenWindow()
+CWSDialog.Initialize('Save all open images to new ZEN Connect workspace - Version: ' + str(version))
+CWSDialog.AddFolderBrowser('projectfolder', 'ZEN Connect Project Target Folder:', initialpath)
+CWSDialog.AddCheckbox('copy', 'Copy Images to new CWS Data Folder', False)
+CWSDialog.AddCheckbox('addatt', 'Include Preview or Label Images (if found)', False)
+CWSDialog.AddCheckbox('closecws', 'Close CWS project at the end', False)
+sf = CWSDialog.Show()
+
+if sf.HasCanceled:
+    message = 'Macro was canceled by user.'
+    print(message)
+    raise SystemExit
+
+targetprojectfolder = str(sf.GetValue('projectfolder'))
+print('Using project target folder: ', targetprojectfolder)
+addpreview = sf.GetValue('addatt')
+copydata = sf.GetValue('copy')
+closecwsproject = sf.GetValue('closecws')
+
+# create CWS project name based on date
+projectname = strftime('ZEN_Connect_%Y-%m-%d_%H-%M', gmtime())
+fullprojectname = Path.Combine(targetprojectfolder, projectname) + ".a5proj"
+print('Created new project name: ', fullprojectname)
+
+# Create the project in the folder below 
+NewProject = ZenConnect.CreateZenConnectDocument(fullprojectname)
+cwsdata_path = Path.Combine(targetprojectfolder, projectname) + '_data'
+
+# get all open documents
+opendocs = Zen.Application.Documents
+
+for doc in opendocs:
+  
+    if doc.IsZenImage and doc.Name != 'Navigation':
+        image = Zen.Application.Documents.GetByName(doc.Name)
+                
+        if copydata:
+            File.Copy(image.FileName, Path.Combine(cwsdata_path, image.Name))
+            print('Copied File: ', image.FileName, ' to: ', cwsdata_path)
+            NewProject.AddImageToZenConnectDocument(Path.Combine(cwsdata_path, image.Name))
+        else:
+            NewProject.AddImageToZenConnectDocument(image.FileName)
+     
+        print('Added image to ZEN Connect: ', image.Name)
+        
+        # check for labels or preview images
+        if addpreview:
+            try:
+                att = image.Core.Attachments.GetEnumerator()
+                index = 0
+                
+                index_to_remove = -1
+                for a in att:
+                    
+                    if a.Name == 'SlidePreview':
+                        print(a.Name)
+                        name = Path.Combine(cwsdata_path, image.NameWithoutExtension + '_Preview.czi')
+                        GetImageDocument(a, name)
+                        preview = Zen.Application.LoadImage(name)
+                        print('Save slide image here: ', name)
+                        preview.Close()
+                        NewProject.AddImageToZenConnectDocument(name)
+                        print('Added image to ZEN Connect: ', name)
+                        
+                    if a.Name == 'Label':
+                        print(a.Name)
+                        name = Path.Combine(cwsdata_path, image.NameWithoutExtension + '_Label.czi')
+                        GetImageDocument(a, name)
+                        print('Save label image here: ', name)
+                        NewProject.AddImageToZenConnectDocument(name)
+                        print('Added image to ZEN Connect: ', name)
+                        
+                    index = index + 1
+                    
+            except ApplicationException as e:
+                print('Application Exception : ', e.Message)
+
+# save CSW project at the end
+NewProject.SaveZenConnectDocument()
+
+# close CWS project at the end
+if closecwsproject:
+    NewProject.CloseZenConnectDocument(False)
