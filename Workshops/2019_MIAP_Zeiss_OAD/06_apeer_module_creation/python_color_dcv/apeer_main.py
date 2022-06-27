6b3681f290ff35fe6e886b9b40f186f0c255431f
ZEISS Microscopy <github.microscopy@zeiss.com>
2022-06-27 15:48:16 +0200

    Work in progress (#28)
    * removed workshops folder
    
    * removed docs section from ML part
    
    * added denoising section
    
    * updates
    
    * updates
    
    * Update README.md
    
    Co-authored-by: Sebastian Rhode <3833249+sebi06@users.noreply.github.com>

--- Workshops/2019_MIAP_Zeiss_OAD/06_apeer_module_creation/python_color_dcv/apeer_main.py
+++
@@ -1,9 +0,0 @@
-﻿from apeer_dev_kit import adk
-import color_dcv
-
-if __name__ == "__main__":
-
-    inputs = adk.get_inputs()
-    outputs = color_dcv.run(inputs['ihc_image'])
-    adk.set_file_output('processed_images', outputs['processed_images'])
-    adk.finalize()
