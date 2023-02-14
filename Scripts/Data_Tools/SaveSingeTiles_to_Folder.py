80ac4fa869f9ee3e804c54504ad07a095b9b744f
ZEISS Microscopy <github.microscopy@zeiss.com>
2023-02-14 11:25:00 +0100

    Update SaveSingeTiles_to_Folder.py
    Added option to also split channels

--- Scripts/Data_Tools/SaveSingeTiles_to_Folder.py
+++ Scripts/Data_Tools/SaveSingeTiles_to_Folder.py
@@ -1,15 +1,15 @@
 #################################################################
 # File       : SaveSingleTiles_to_Folder.py
-# Version    : 1.3
+# Version    : 1.4
 # Author     : czsrh
-# Date       : 12.11.2022
+# Date       : 14.02.2023
 # Insitution : Carl Zeiss Microscopy GmbH
 #
 # Save the selected image as single tiles into the specified folder
 # using the selected format.
 # When using OME-TIFF export make sure one used a valid setting
 #
-# Copyright(c) 2022 Carl Zeiss AG, Germany. All Rights Reserved.
+# Copyright(c) 2023 Carl Zeiss AG, Germany. All Rights Reserved.
 #
 # Permission is granted to use, modify and distribute this code,
 # as long as this copyright notice remains part of the code.
@@ -17,7 +17,7 @@
 
 from System.IO import Path, File, Directory, FileInfo
 
-version = 1.3
+version = 1.4
 
 # clear output
 Zen.Application.MacroEditor.ClearMessages()
@@ -62,9 +62,10 @@ imgfolder = Path.Combine(Zen.Application.Environment.GetFolderPath(ZenSpecialFol
 # Initialize the dialog window for the required user inputs
 wd = ZenWindow()
 wd.Initialize('Save Tiles as Single Images - Version: ' + str(version))
-wd.AddDropDown('czi', 'Select CZI Image Data', CZIfiles_short, 0, '0', '0')
-wd.AddFolderBrowser('savefolder', 'Specify Save Folder', imgfolder, '1', '0')
-wd.AddDropDown('saveformat', 'Select Image Format', formats, 0, '2', '0')
+wd.AddDropDown('czi', 'Select CZI Image Data', CZIfiles_short, 0)
+wd.AddCheckbox('split_channels', 'Split Channels', False)
+wd.AddFolderBrowser('savefolder', 'Specify Save Folder', imgfolder)
+wd.AddDropDown('saveformat', 'Select Image Format', formats, 0)
 
 # show the window
 result = wd.Show()
@@ -79,6 +80,7 @@ if result.HasCanceled:
 ft = result.GetValue('saveformat')
 cziname = result.GetValue('czi')
 SavePath = result.GetValue('savefolder')
+split_ch = result.GetValue('split_channels')
 print('Saving CZI : ', cziname, ' as ', ft)
 # get the selected image
 img = Zen.Application.Documents.GetByName(Path.GetFileNameWithoutExtension(cziname))
@@ -94,6 +96,11 @@ try:
     nTiles = img.Bounds.SizeM
 except:
     nTiles = 1
+    
+try:
+    nChannels = img.Bounds.SizeC
+except:
+    nChannels = 1
 
 print('Number of Scenes detected: ', nScenes)
 print('Number of Tiles detected: ', nTiles)
@@ -106,33 +113,69 @@ ometiff_setting = Zen.Processing.Utilities.Settings.OmeTiffExportSetting()
 ometiff_setting.Load(settingsfile, ZenSettingDirectory.User)
 
 
-# address each tile image in Zen without opening it
-for s in range(0, nScenes):
-    for m in range(0, nTiles):
+if split_ch:
 
-        # get the strings with leading zeros for splitting
-        s_str = addzeros(s + 1)
-        m_str = addzeros(m + 1)
+    # address each tile image in Zen without opening it
+    for s in range(0, nScenes):
+        for m in range(0, nTiles):
+            for c in range(0, nChannels):
+    
+                # get the strings with leading zeros for splitting
+                s_str = addzeros(s + 1)
+                m_str = addzeros(m + 1)
+                c_str = addzeros(c + 1)
+        
+                # create the subimage
+                imgTile = img.CreateSubImage('S(' + s_str + ')|M(' + m_str + ')|C(' + c_str + ')')
+        
+                # create a useful name
+                s_str = addzeros(s)
+                m_str = addzeros(m)
+                c_str = addzeros(c)
+                
+                if ft == "*.ome.tiff":
+                    imgTile.Name = nameParent[:-4] + '_S' + s_str + '_T' + m_str + '_C' + c_str
+                    print(imgTile.Name)
+                    # save current tile with the specified format
+                    Zen.Processing.Utilities.ExportOmeTiff(imgTile, ometiff_setting, imgTile.Name, SavePath)
+                
+                else:
+                    imgTile.Name = nameParent[:-4] + '_S' + s_str + '_T' + m_str +'_C' + c_str + ft[1:]
+                    print(imgTile.Name)
+                    # save current tile with the specified format
+                    Zen.Application.Save(imgTile, Path.Combine(SavePath, imgTile.Name), False)
+                
+                imgTile.Close()
+                
+if not split_ch:
 
-        # create the subimage
-        imgTile = img.CreateSubImage('S(' + s_str + ')|M(' + m_str + ')')
-
-        # create a useful name
-        s_str = addzeros(s)
-        m_str = addzeros(m)
+    # address each tile image in Zen without opening it
+    for s in range(0, nScenes):
+        for m in range(0, nTiles):
+    
+                # get the strings with leading zeros for splitting
+                s_str = addzeros(s + 1)
+                m_str = addzeros(m + 1)
         
-        if ft == "*.ome.tiff":
-            imgTile.Name = nameParent[:-4] + '_S' + s_str + '_T' + m_str
-            print(imgTile.Name)
-            # save current tile with the specified format
-            Zen.Processing.Utilities.ExportOmeTiff(imgTile, ometiff_setting, imgTile.Name, SavePath)
+                # create the subimage
+                imgTile = img.CreateSubImage('S(' + s_str + ')|M(' + m_str + ')')
         
-        else:
-            imgTile.Name = nameParent[:-4] + '_S' + s_str + '_T' + m_str + ft[1:]
-            print(imgTile.Name)
-            # save current tile with the specified format
-            Zen.Application.Save(imgTile, Path.Combine(SavePath, imgTile.Name), False)
-        
-        imgTile.Close()
+                # create a useful name
+                s_str = addzeros(s)
+                m_str = addzeros(m)
+                
+                if ft == "*.ome.tiff":
+                    imgTile.Name = nameParent[:-4] + '_S' + s_str + '_T' + m_str
+                    print(imgTile.Name)
+                    # save current tile with the specified format
+                    Zen.Processing.Utilities.ExportOmeTiff(imgTile, ometiff_setting, imgTile.Name, SavePath)
+                
+                else:
+                    imgTile.Name = nameParent[:-4] + '_S' + s_str + '_T' + m_str + ft[1:]
+                    print(imgTile.Name)
+                    # save current tile with the specified format
+                    Zen.Application.Save(imgTile, Path.Combine(SavePath, imgTile.Name), False)
+                
+                imgTile.Close()
 
 print('All Tiles saved.')
